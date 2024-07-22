59a7bc0b7c57fb9696a7f1ec53de5748a570e49d
Paulina-Pereyra <paulinapereyrasantana@gmail.com>
2024-07-22 19:44:06 +0000

    family static API

--- src/app.py
+++ src/app.py
@@ -25,18 +25,47 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+# Ruta para obtener todos los miembros
 @app.route('/members', methods=['GET'])
 def handle_hello():
-
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
-
-
+    response_body = members
     return jsonify(response_body), 200
+
+# Ruta para obtener un miembro específico por su ID
+@app.route('/member/<int:id>', methods=['GET'])
+def get_member(id):
+    try:
+        member = jackson_family.get_member(id)
+        if member:
+            return jsonify(member), 200
+        else:
+             return jsonify({"error": "Member not found"}), 400    
+    except:
+        return jsonify({"msg": "Internal server error"}), 500
+
+# Ruta para agregar un nuevo miembro
+@app.route('/member', methods=['POST'])
+def add_a_member():
+    request_body = request.get_json()
+    if not request_body:
+        return jsonify({"msg": "No request body"}), 400
+    jackson_family.add_member(request_body)
+    return jsonify ("member added"), 200
+
+@app.route('/member/<int:id>', methods=['DELETE'])
+def delete_member(id):
+    try: 
+        deleted= jackson_family.delete_member(id)
+        if deleted:
+            return jsonify ({"done": True}), 200
+        else:
+            return jsonify ({"done": False}), 400
+    except:
+        return jsonify({"msg":"Internal server error"}), 500
+               
+    
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
